7b1fd146fb6ed04557fe7e75c3653d352b97eab8
tronje <tronjekrabbe@gmail.com>
2013-06-08 01:28:14 +0200

    first draft

---
+++ main.js
@@ -0,0 +1,29 @@
+/**
+ *  Main JS
+ */
+function shit() {
+
+  var player = new jaws.Rect(50, 50, 50, 50);
+  var speed = 2;
+
+  this.update = function() {
+    if (jaws.pressed("a")) {player.move(-speed, 0)};
+    if (jaws.pressed("d")) {player.move(speed, 0)};
+    if (jaws.pressed("s")) {player.move(0, speed)};
+    if (jaws.pressed("w")) {player.move(0, -speed)};
+    if (jaws.pressed("up")) {speed++};
+    if (jaws.pressed("down")) {speed--};
+  }
+
+  this.draw = function() {
+    jaws.clear();
+    player.draw();
+  }
+
+}
+
+
+window.onload = function() {
+  jaws.start(shit);
+}
+var c = document.getElementById("canvas");
