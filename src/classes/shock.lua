f7cf647b218d6e90d902c4685b7ae0a033fc2e13
C-gotoh <shigotoh1@gmail.com>
2013-06-08 11:11:05 +0200

    shock

--- src/classes/shock.lua
+++ src/classes/shock.lua
@@ -3,8 +3,9 @@ Shock = class("Shock")
 function Shock:fire(force)
 	self.x = love.mouse.getX()
 	self.y = love.mouse.getY()
+	self.forcec = force
 	for index, value in pairs(levelone.el) do
-		force = (1/(distance(value.body:getX(), value.body:getY(), self.x, self.y)^2))
-		value.body:applyForce((force*math.abs(value.x-self.x)), (force*math.abs(value.y - self.y)))
+		self.forcec = (1/(distance(value.body:getX(), value.body:getY(), self.x, self.y)))
+		value.body:applyForce((self.forcec*(value.x-self.x)), (self.forcec*(value.y - self.y)))
 	end
 end
