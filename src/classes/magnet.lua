fbe1b94cf70ad6ce507387ea71211b7d0e3dc365
C-gotoh <shigotoh1@gmail.com>
2013-06-08 04:55:06 +0200

    addforce

--- src/classes/magnet.lua
+++ src/classes/magnet.lua
@@ -1,3 +1,26 @@
 function Magnet:__init(world, x, y, force)
     self.body = love.physics.newBody(world, x, y, static)
 end
+
+function Magnet:addforce(aX,aY,bX,bY)
+    local posA = a.get.pos
+    local posB = b.get.pos
+    local abstand = sqrt((posA.getX - posB.getX)^2 + (posA.getY - posB.getY)^2)
+
+    if abstand < ((math.pi/2)*force)
+        laenge = math.cos (abstand * (1/force))
+        forcevectorx = laenge*(bX - aX)
+        forcevectory = laenge*(bY - aY)
+        newvectorx = aX.getVelocity + forcevectorx
+        newvectory = aY.getVelocity + forcevectory
+
+    end
+end
+
+function Magnet:update(dt)
+
+end
+
+function Magnet:draw()
+    love.graphic.draw(resources.graphic.electron, self.body:getX, self.body:getY)
+end
