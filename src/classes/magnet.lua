e83790974c10c1c3bec527cfb2b71824b87031d1
C-gotoh <shigotoh1@gmail.com>
2013-06-08 20:45:34 +0200

    magnets show their range

--- src/classes/magnet.lua
+++ src/classes/magnet.lua
@@ -3,9 +3,11 @@ Magnet = class("Magnet")
 function Magnet:__init(world, x, y, r, fr, force, typ)
     self.body = love.physics.newBody(world, x, y, static)
     self.shape = love.physics.newCircleShape(r)
+    self.radius = r
     self.fixture = love.physics.newFixture(self.body, self.shape)
     self.fixture:setUserData(self)
     self.fr = fr -- range
+    self.rangevisual = self.fr
     self.force = force
     self.type = typ
     self.color_fac = force / 12
@@ -29,10 +31,16 @@ function Magnet:addForce(object)
 end
 
 function Magnet:update(dt)
+    if self.rangevisual < self.radius then
+        self.rangevisual = self.fr
+    end
+    
+    self.rangevisual = self.rangevisual - 1
 
 end
 
 function Magnet:draw()
+
     if self.type == "Electron" then
         love.graphics.setColor(255, 37, 0,  self.color_fac * 255)
         love.graphics.circle("fill", self.body:getX(), self.body:getY(), 25)
@@ -46,6 +54,11 @@ function Magnet:draw()
         love.graphics.circle("fill", self.body:getX(), self.body:getY(), 15)
         love.graphics.setColor(255, 37, 0, self.color_fac * 80)
         love.graphics.circle("fill", self.body:getX(), self.body:getY(), 8)
+
+        --visualize
+        love.graphics.setColor(255, 37, 0, self.color_fac * 80)
+        love.graphics.circle("line", self.body:getX(), self.body:getY(), self.rangevisual)
+
     elseif self.type == "Proton" then
         love.graphics.setColor(56 + (self.color_fac * 0.1), 222 + (self.color_fac * 0.1), 255 + (self.color_fac * 0.1), self.color_fac * 255)
         love.graphics.circle("fill", self.body:getX(), self.body:getY(), 25)
@@ -59,5 +72,9 @@ function Magnet:draw()
         love.graphics.circle("fill", self.body:getX(), self.body:getY(), 15)
         love.graphics.setColor(56, 222, 255, self.color_fac * 80)
         love.graphics.circle("fill", self.body:getX(), self.body:getY(), 8)
+
+        --visualize
+        love.graphics.setColor(56, 222, 255, self.color_fac * 80)
+        love.graphics.circle("line", self.body:getX(), self.body:getY(), self.rangevisual)
     end 
 end
