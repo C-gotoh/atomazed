b4f79e51a7ae09323379bbbf1bf889be785041f3
Arne Beer <arne@twobeer.de>
2013-06-09 12:49:17 +0200

    Colors in Intro changed

--- src/states/IntroState.lua
+++ src/states/IntroState.lua
@@ -13,9 +13,9 @@ function IntroState:draw()
 	love.graphics.print("Press \"R\" for Restart", 50, 50 )
 	love.graphics.setColor(200, 50, 0, 255)
 	love.graphics.print("Press 'Left mousebutton' to create a shockwave  \n or to place a Proton Tower", 50, 150 )	
-	love.graphics.setColor(50, 200, 50, 255)
-	love.graphics.print("Press 'Right mousebutton' to place a Electron Tower", 50, 250 )
 	love.graphics.setColor(0, 50, 200, 255)
+	love.graphics.print("Press 'Right mousebutton' to place a Electron Tower", 50, 250 )
+	love.graphics.setColor(50, 200, 50, 255)
 	love.graphics.print("Press middle 'Mousebutton' or 'Space' \n to switch Mousemode  ", 50, 350 )	
 end
 
