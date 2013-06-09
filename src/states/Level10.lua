d443571d1df52cee01a42e2f362802f78c4a9774
Hans Ole Hatzel <hhatzel@gmail.com>
2013-06-09 16:12:58 +0200

    Moved text in overlay

--- src/states/Level10.lua
+++ src/states/Level10.lua
@@ -20,8 +20,8 @@ Level10 = class("Level10", Level)
 function Level10:__init()
     self.__super.__init(self)
     self.force = 0
-    self.index = 1
-    self.string = {"Repulsion can be a good thing.", 100, 450}
+    self.index = 10
+    self.string = {"Remember you can place Magnets, just press spacebar.", 200, 100}
 end
 
 function Level10:load()
@@ -57,39 +57,31 @@ function Level10:load()
     wall = Wall(world, 0, 300, 4, 600, "static")
     table.insert(self.walls, wall)
 
-    local cwall = ColoredWall(world, love.graphics.getWidth()/2, 100, 20, 200, "static", 100, 100, 100, 50)
-    table.insert(self.walls, cwall)
 
-    local cwall = ColoredWall(world, love.graphics.getWidth()/2, 500, 20, 200, "static", 100, 100, 100, 50)
-    table.insert(self.walls, cwall)
-
-    local proton = Proton(world, 733, 434)
-    proton.body:setLinearVelocity(0, 10)
-    table.insert(self.proton, proton)
-
-    local proton = Proton(world, 769, 315)
-    proton.body:setLinearVelocity(0, 10)
-    table.insert(self.proton, proton)
-
-
-
-    local el = Electron(world, 213, 200)
-    proton.body:setLinearVelocity(0, 5)
-    table.insert(self.el, el)
-
-    local el = Electron(world, 153, 100)
-    proton.body:setLinearVelocity(0, 5)
+    local el = Electron(world,100, 350)
     table.insert(self.el, el)
 
     self.darkness = 0 
-    self.maxElectrons = 2
-    self.minElectrons = 2
+    self.maxElectrons = 1
+    self.minElectrons = 0
     self.endtimer = 0
 
-    self.limitshock = 3
-    self.magnetlimitp = 1
-    self.magnetlimite = 1
+    self.limitshock = 1
+    self.magnetlimitp = 2
+    self.magnetlimite = 0
 
-    local magnet = Magnet(world, 350, 300, 20, 200, 12, "Proton")
-    table.insert(self.magnet, magnet)
+    local cwall = ColoredWall(world, 512-15, 200, 30, 400, "static", 100, 100, 100, 50)
+    table.insert(self.walls, cwall)
+
+    local proton = Proton(world, 720, 120)
+    table.insert(self.proton, proton)
+
+    local proton = Proton(world, 750, 35)
+    table.insert(self.proton, proton)
+
+    local proton = Proton(world, 763, 100)
+    table.insert(self.proton, proton)
+
+    local proton = Proton(world, 900, 35)
+    table.insert(self.proton, proton)
 end
